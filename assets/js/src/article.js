a96fc7d06d279d642a26b63bbbc4fa550710fb15
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-05-27 18:03:05 +0200

    Fixed a JS issue on non-article page

--- assets/js/src/article.js
+++ assets/js/src/article.js
@@ -16,6 +16,43 @@
     if (this.conf.tableOfContent === true) { 
       this.createTableOfContent();
     }
+  
+    if (
+      window.matchMedia('(min-width: 750px').matches
+      && document.querySelector('.post-date')
+    ) {
+      var gridify = this.gridifyImages.bind(this);
+      window.addEventListener('resize', gridify);
+      gridify();
+    }
+  };
+
+  Article.prototype.gridifyImage = function (image, containerWidth) {
+    var i = new Image();
+    i.onload = function () {
+      var height = i.height;
+      var width = Math.min(i.width, containerWidth);
+      var height = i.width > containerWidth
+        ? containerWidth * i.height / i.width
+        : i.height;
+      var roundedHeight = (Math.round(height / 40) * 40);
+
+      image.style.width = width + 'px';
+      image.style.height = roundedHeight + 'px';
+    };
+    i.src = image.src;
+  };
+
+  Article.prototype.gridifyImages = function () {
+    var images = document.querySelectorAll('img');
+    var containerWidth = document
+      .querySelector('.container')
+      .offsetWidth - (40 * 2);
+
+    var gridify = this.gridifyImage;
+    Array.prototype.forEach.call(images, function (image) {
+      gridify(image, containerWidth);
+    });
   };
   
   Article.prototype.createSections = function () {
