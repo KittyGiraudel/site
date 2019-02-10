4b550aeee90d8fe8e90e4bd1412b57d41443ef30
Hugo Giraudel <hugo.giraudel@gmail.com>
2019-02-10 19:00:52 +0100

    Remove Google Analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,13 @@
 ;(function(global) {
-  var ANALYTICS_URL = '//www.google-analytics.com/ga.js'
   var ADS_URL = '//engine.carbonads.com/z/24598/azcarbon_2_1_0_HORIZ'
   var CODEPEN_URL = '//codepen.io/assets/embed/ei.js'
   var SASSMEISTER_URL = '//static.sassmeister.com/js/embed.js'
-  var ANALYTICS_OPTIONS = [['_setAccount', 'UA-30333387-2'], ['_trackPageview']]
 
   function $(selector, context) {
     var nodes = (context || document).querySelectorAll(selector)
     if (!nodes.length) return null
     if (nodes.length === 1) return nodes[0]
     return Array.prototype.slice.call(nodes)
-  }
-
-  function loadAnalytics(callback) {
-    global._gaq = ANALYTICS_OPTIONS
-    loadJS(ANALYTICS_URL, callback)
   }
 
   function loadAds(callback) {
@@ -29,8 +22,7 @@
     $('.sassmeister') && loadJS(SASSMEISTER_URL, callback)
   }
 
-  function App(options) {
-    loadAnalytics()
+  function App() {
     loadAds()
     loadSassMeister()
     loadCodePen()
