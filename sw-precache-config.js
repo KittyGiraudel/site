489c122f9ebea44980992516a95737d0df5605d2
Hugo “Kitty” Giraudel <hugo.giraudel@gmail.com>
2020-11-28 14:02:24 +0100

    Migrate from sw-precache to workbox-cli

--- sw-precache-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  staticFileGlobs: ['_site/**/*.html', '_site/assets/css/*.css'],
-  runtimeCaching: [
-    {
-      urlPattern: /^https?:\/\/\w+.cloudfront.net\/bundles\/\w+.js$/,
-      handler: 'cacheFirst'
-    }
-  ]
-}
