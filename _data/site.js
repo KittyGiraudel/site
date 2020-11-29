07853e378a67919fed02028f02c1e51715c4c900
Hugo “Kitty” Giraudel <hugo.giraudel@gmail.com>
2020-11-29 16:46:54 +0100

    Restore ‘lastBuildDate’ in RSS feed

--- _data/site.js
+++ _data/site.js
@@ -2,5 +2,6 @@ module.exports = {
   "url": "https://hugogiraudel.com",
   "description": "I’m Hugo “Kitty” Giraudel, a non-binary web developer based in Berlin, focused on accessibility and inclusivity.",
   "author": "Hugo “Kitty” Giraudel",
-  "environment": process.env.NODE_ENV
+  "environment": process.env.NODE_ENV,
+  "time": new Date()
 }
