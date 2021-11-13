6f8b193a43c9646cd68033f4d87f1c18d2124f4f
Kitty Giraudel <kitty.giraudel@gmail.com>
2021-11-13 12:27:55 +0100

    Add interaction for the dark mode toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,26 @@ document.addEventListener('DOMContentLoaded', function () {
         link.dataset.visited = true
     })
   })()
+
+  const modeToggle = document.querySelector('#dark-mode-toggle')
+  const savedState = JSON.parse(localStorage.getItem('dark-mode') || 'false')
+
+  document.documentElement.classList[savedState ? 'add' : 'remove']('dark')
+  modeToggle.setAttribute('aria-pressed', savedState)
+  modeToggle.removeAttribute('hidden')
+
+  modeToggle.addEventListener('click', function (event) {
+    const target = event.target.closest('button')
+    const current = JSON.parse(target.getAttribute('aria-pressed')) === true
+
+    if (current) {
+      document.documentElement.classList.remove('dark')
+      target.setAttribute('aria-pressed', false)
+      localStorage.setItem('dark-mode', false)
+    } else {
+      document.documentElement.classList.add('dark')
+      target.setAttribute('aria-pressed', true)
+      localStorage.setItem('dark-mode', true)
+    }
+  })
 })
