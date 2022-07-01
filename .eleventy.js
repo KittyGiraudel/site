f7efb2250180d0ddfb84ee7a02d497197c1cf60a
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-07-01 15:06:59 +0200

    Update favicon

--- .eleventy.js
+++ .eleventy.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
   config.addPassthroughCopy('_headers')
   config.addPassthroughCopy('humans.txt')
   config.addPassthroughCopy('manifest.json')
-  config.addPassthroughCopy('apple-touch-icon.png')
+  config.addPassthroughCopy('apple-touch-icon.jpg')
   config.addPassthroughCopy('favicon.ico')
 
   // CSS and JavaScript are inlined in HTML for performance reasons. The problem
