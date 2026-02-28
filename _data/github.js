0cad33a182907a12a647191c9f8773d4ba33a638
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-02-28 18:34:13 +0100

    Fetch GitHub stargazers at build time

---
+++ _data/github.js
@@ -0,0 +1,23 @@
+import Fetch from "@11ty/eleventy-fetch";
+
+export default async function () {
+  const repositories = [
+    "https://api.github.com/repos/KittyGiraudel/a11y-dialog",
+    "https://api.github.com/repos/KittyGiraudel/SJSJ",
+    "https://api.github.com/repos/KittyGiraudel/sass-guidelines",
+  ];
+
+  const repositoriesData = await Promise.all(repositories
+    .map(url => Fetch(url, { duration: "1w", type: "json" }))
+  )
+
+  const data = (repositoriesData)
+    .reduce((acc, repo) => {
+      acc[repo.clone_url.replace('.git', '')] = repo.stargazers_count;
+      return acc;
+    }, {})
+
+  return {
+    stargazers: data
+  };
+};
