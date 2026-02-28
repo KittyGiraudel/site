4df25dd2905256567ff40717e54a61b1867a2abd
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-02-28 10:48:49 +0100

    Update the workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  swDest: '_site/service-worker.js',
-  globPatterns: ['_site/**/*.html', '_site/assets/css/*.css'],
-  runtimeCaching: [
-    {
-      urlPattern: /^https?:\/\/\w+.cloudfront.net\/bundles\/\w+.js$/,
-      handler: 'StaleWhileRevalidate'
-    }
-  ]
-}
+	globDirectory: '_site',
+	globPatterns: [
+		'**/*.{js,txt,html,ico,jpg,png,css,jpeg,gif,svg}'
+	],
+	swDest: '_site/service-worker.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	],
+};
