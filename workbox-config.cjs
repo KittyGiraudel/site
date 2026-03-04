dc4fa9a58ca9b71b50522d259295e160ce756a64
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-03-04 11:47:54 +0100

    No longer cache HTML pages with the service worker

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   globDirectory: '_site',
-  globPatterns: ['**/*.{js,txt,html,ico,jpg,png,css,jpeg,gif,svg}'],
+  // Only precache static assets. HTML is fetched from the network so that new
+  // content (like freshly published articles) is visible on a regular reload.
+  globPatterns: ['**/*.{js,txt,ico,jpg,png,css,jpeg,gif,svg}'],
   swDest: '_site/service-worker.js',
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 }
